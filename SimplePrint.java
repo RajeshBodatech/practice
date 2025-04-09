20590451d4b5cded1b85e82746cd8aa53333018b
Boda Rajesh <154888255+RajeshBodatech@users.noreply.github.com>
2025-04-09 21:18:34 +0530

    Update SimplePrint.java

--- SimplePrint.java
+++ SimplePrint.java
@@ -2,5 +2,6 @@ import java.util.*;
 public class SimplePrint {
     public static void main(String[] args) {
          System.out.println("This is a simple print statement in Java.");
+        System.out.println("This is Boda Rajesh from telangana.");
     }
 }
