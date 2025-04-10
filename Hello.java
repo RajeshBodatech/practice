0d2bef3fa86d086fcbcb7d0edb273e9f144d7c28
RajeshBodatech <rajeshboda30@gmail.com>
2025-04-10 09:29:50 +0530

    java file

--- Hello.java
+++ Hello.java
@@ -4,6 +4,6 @@ public class Hello
 {
 public static void main(String args[])
 {
-System.out.println("Hello all this is Rajesh Boda from CSE rgukt Basar:");
+System.out.println("Hello all this is Rajesh Boda from CSE rgukt Basar welcome:");
 }
 }
